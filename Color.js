3c80647b362ceec7d9e7220313e570e62b05d714
GacorDanGG <144035471+GacorDanGG@users.noreply.github.com>
2024-06-24 19:58:25 +0700

    Delete Color.js

--- Color.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('input[name="color"]').forEach((radio) => {
-    radio.addEventListener('change', function() {
-        document.querySelectorAll('input[name="color"] + label').forEach((label) => {
-            label.style.border = '2px solid transparent';
-        });
-        this.nextElementSibling.style.border = '2px solid black';
-    });
-});
